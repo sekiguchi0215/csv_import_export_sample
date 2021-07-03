db729d73470d591324185611edb7a7a29473938d
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-03 17:27:55 +0900

    インポート機能のコントローラーを作成 及び ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: :index
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :index do
+    collection { resource :import, only: :create, controller: :import }
+  end
 end
