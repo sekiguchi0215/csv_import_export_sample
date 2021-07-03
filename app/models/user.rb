95a088773eb5b0a307aafb56465568bd13918d87
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-03 18:04:15 +0900

    エラー解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  CSV_COLUMNS = %w[name age height].freeze
+
   def self.import_csv(file:)
+    list = []
     CSV.foreach(file.path, headers: true) do |row|
-      create!(row.to_h)
+      list << row.to_h.slice(*CSV_COLUMNS)
     end
+    User.import!(list)
   end
 end
