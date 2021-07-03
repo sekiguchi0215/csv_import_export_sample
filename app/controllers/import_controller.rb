141719aa29a7123cb5db0828eb00c26627c266e4
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-03 17:39:22 +0900

    インポート機能を実装

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,4 +1,9 @@
 class ImportController < ApplicationController
   def create
+    file = params[:file]
+    CSV.foreach(file.path, headers: true) do |row|
+      User.create!(row.to_h)
+    end
+    redirect_to users_path
   end
 end
