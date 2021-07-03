cbd2c31be6f723d609a4d5e0a600169fbd1cd42b
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-03 17:42:23 +0900

    Import コントローラーのファクタリング

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,9 +1,6 @@
 class ImportController < ApplicationController
   def create
-    file = params[:file]
-    CSV.foreach(file.path, headers: true) do |row|
-      User.create!(row.to_h)
-    end
+    User.import_csv(file: params[:file])
     redirect_to users_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  def self.import_csv(file:)
+    CSV.foreach(file.path, headers: true) do |row|
+      create!(row.to_h)
+    end
+  end
 end
